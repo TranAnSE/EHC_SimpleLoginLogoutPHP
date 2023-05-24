05b62e12861e9161431e2a353ae29c5bc105f668
TranAnSE <ankhanh47837.hoctot@gmail.com>
2023-05-24 23:58:40 +0700

    Big Update
    Readme for more information

--- login.php
+++ login.php
@@ -3,29 +3,31 @@
 $is_invalid = false; //Flag Variable
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") { //Checking form sent?
-    
-    $mysqli = require __DIR__ . "/database.php"; //Including MySQL database
-    
+
+    $mysqli = require __DIR__ . "/config/database.php"; //Including MySQL database
+
     //Select users table from database then query
-    $sql = sprintf("SELECT * FROM users
+    $sql = sprintf(
+        "SELECT * FROM users
                     WHERE username = '%s'",
-                   $mysqli->real_escape_string($_POST["username"]));
-    
+        $mysqli->real_escape_string($_POST["username"])
+    );
+
     $result = $mysqli->query($sql);
-    
+
     $user = $result->fetch_assoc();
-    
+
     //Validate username and password
     if ($user) {
-        
+
         if (password_verify($_POST["password"], $user["password_hash"])) {
-            
+
             session_start();
-            
+
             session_regenerate_id();
-            
+
             $_SESSION["user_id"] = $user["id"];
-            
+
             header("Location: index.php");
             exit;
         }
@@ -33,35 +35,77 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") { //Checking form sent?
     //Flag Successful
     $is_invalid = true;
 }
-    //Just Simple UI
+
 ?>
+<!-- Signin  Form -->
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Login</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2.1.1/out/water.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign in</title>
+
+    <!-- Font Icon -->
+    <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+    <!-- Main css -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
-    <h1>Login</h1>
-<!-- Respond Error if Username or Password incorrect -->
-    <?php if ($is_invalid): ?>
-        <em>Username or Password may be incorrect</em>
-    <?php endif; ?>
-    <!-- Simple form to be fill data to login -->
-    <form method="post">
-        <label for="username">Username</label>
-        <input type="username" name="username" id="username"
-               value="<?= htmlspecialchars($_POST["username"] ?? "") ?>">
-        
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password">
-        <!-- Submit Button -->
-        <button>Log in</button>
-        <!-- Additional line -->
-        <p>Don't have an account? <a href="signup">Click here</a></p>
-    </form>
-    
-</body>
+    <div class="main">
+        <section class="sign-in">
+            <div class="container">
+                <div class="signin-content">
+                    <div class="signin-image">
+                        <figure><img src="images/signin-image.jpg" alt="sing up image"></figure>
+                        <a href="signup" class="signup-image-link">Create an account</a>
+                    </div>
+
+                    <div class="signin-form">
+                        <h2 class="form-title">Sign in</h2>
+                        <form method="POST" class="register-form" id="login-form">
+                            <div class="form-group">
+                                <label for="username"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="username" id="username" placeholder="Username" value="<?= htmlspecialchars($_POST["username"] ?? "") ?>" />
+                            </div>
+                            <div class="form-group">
+                                <label for="password"><i class="zmdi zmdi-lock"></i></label>
+                                <input type="password" name="password" id="password" placeholder="Password" />
+                            </div>
+                            <!-- Respond Error if Username or Password incorrect -->
+                            <?php if ($is_invalid) : ?>
+                                <em>Username or Password may be incorrect</em>
+                            <?php endif; ?>
+
+                            <div class="form-group">
+                                <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
+                                <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember
+                                    me</label>
+                            </div>
+                            <div class="form-group form-button">
+                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in" />
+                            </div>
+                        </form>
+                        <div class="social-login">
+                            <span class="social-label">Or login with</span>
+                            <ul class="socials">
+                                <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
+                                <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
+                                <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </div>
+
+    <!-- JS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+
 </html>
--- signup-process.php
+++ signup-process.php
@@ -1,33 +1,5 @@
 <?php
 //Policy require for Signup Process
-if (empty($_POST["name"])) {
-    die("Name is required");
-}
-
-if (preg_match("/([%\$#\*@]+)/", $_POST["name"])) {
-    die("Your name may not contain a special character. Please try again.");
-}
-
-if (preg_match("/([%\$#\*@]+)/", $_POST["username"])) {
-    die("Your username may not contain a special character. Please try again.");
-}
-
-if (strlen($_POST["username"]) < 5) {
-    die("Username must be at least 5 characters");
-}
-
-if (strlen($_POST["password"]) < 8) {
-    die("Password must be at least 8 characters");
-}
-
-if (!preg_match("/[a-z]/i", $_POST["password"])) {
-    die("Password must contain at least one letter");
-}
-
-if (!preg_match("/[0-9]/", $_POST["password"])) {
-    die("Password must contain at least one number");
-}
-
 if ($_POST["password"] !== $_POST["password_confirmation"]) {
     die("Passwords is not matching");
 }
@@ -35,7 +7,7 @@ if ($_POST["password"] !== $_POST["password_confirmation"]) {
 //Hash password via PASSWORD_DEFAULT algorithm for security
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
-$mysqli = require __DIR__ . "/database.php"; //Including MySQL database
+$mysqli = require __DIR__ . "/config/database.php"; //Including MySQL database
 
 //Insert value to database
 $sql = "INSERT INTO users (name, username, password_hash) 
